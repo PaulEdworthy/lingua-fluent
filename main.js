d1162605734283df112fcb5eba7c6b5ea0ca522e
PaulEdworthy <pauledworthy@gmail.com>
2021-07-10 16:02:41 -0400

    build: retrieve JSON data

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-const wordToTranslate = document.getElementById('word');
-const translatedWord = document.getElementById('solution');
 
-wordToTranslate.textContent = 'bonjour';
-translatedWord.textContent = 'hi/hello'; 
+// let number, french, english;
 
+fetch('./json_test.json')
+  .then(response => {
+    return response.json();
+  }).then(data => {
+    console.log(data);
+    
+    // number = data.number;
+    // french = data.rench;
+    // english = data.english; 
+    console.log(data[0]);
+    
+  });
+
+  
+
+const sourceLang = document.getElementById('source-language');
+const targetLang = document.getElementById('target-language');
+
+// sourceLang.textContent = french;
