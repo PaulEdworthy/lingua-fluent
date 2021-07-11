2d47ec3f45ceabe02aaf29b7037e09ee945f3670
PaulEdworthy <pauledworthy@gmail.com>
2021-07-11 11:24:12 -0400

    build: show/hide backside functionality, advance card and increment card count

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
+let number, french, english;
+let cardIteration = 0;
+const jsonUrl = ('./1000_words.json');
+const cardBack = document.getElementById('card__item__back');
+const sourceLang = document.getElementById('source-language');
+const targetLang = document.getElementById('target-language');
+const showAnswer = document.getElementById('btn--show');
+const nextCard = document.getElementById('btn__next');
+const hardChoice = document.getElementById('btn--hard');
+const goodChoice = document.getElementById('btn--good');
+const easyChoice = document.getElementById('btn--easy');
 
-// let number, french, english;
 
-fetch('./json_test.json')
+function getData () {
+  fetch(jsonUrl)
   .then(response => {
     return response.json();
   }).then(data => {
-    console.log(data);
+
+    number = data[cardIteration].Number;
+    french = data[cardIteration].French;
+    english = data[cardIteration].English;
+    console.log(cardIteration);
     
-    // number = data.number;
-    // french = data.rench;
-    // english = data.english; 
-    console.log(data[0]);
-    
+    // console.log(data[0].English);
+    sourceLang.textContent = french;
+    targetLang.textContent = english;
   });
-
+}
+getData();
   
+showAnswer.addEventListener('click', () => {
+  cardBack.classList.toggle('card__item__back');
+});
 
-const sourceLang = document.getElementById('source-language');
-const targetLang = document.getElementById('target-language');
-
-// sourceLang.textContent = french;
+nextCard.addEventListener('click', () => {
+  cardBack.classList.toggle('card__item__back');  
+  cardIteration++;
+  getData();
+});
