ddb9270b59e19e533a88f9ca8981b8a529e1c7e0
PaulEdworthy <pauledworthy@gmail.com>
2021-07-13 15:19:08 -0400

    don't remember....

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const cardBack = document.getElementById('card__item__back');
 const sourceLang = document.getElementById('source-language');
 const targetLang = document.getElementById('target-language');
 const showAnswer = document.getElementById('btn--show');
-const nextCard = document.getElementById('btn__next');
+const nextCard = document.getElementById('btn--next');
 const hardChoice = document.getElementById('btn--hard');
 const goodChoice = document.getElementById('btn--good');
 const easyChoice = document.getElementById('btn--easy');
 
+cardBack.style.display = 'none';
 
 function getData () {
   fetch(jsonUrl)
@@ -17,24 +18,27 @@ function getData () {
     return response.json();
   }).then(data => {
 
+    // remove cardIteration and use random card
+    // let randomWord = Math.floor(Math.random() * 1000);
+    // console.log(randomWord);
+
     number = data[cardIteration].Number;
     french = data[cardIteration].French;
     english = data[cardIteration].English;
-    console.log(cardIteration);
-    
+
     // console.log(data[0].English);
     sourceLang.textContent = french;
     targetLang.textContent = english;
   });
 }
 getData();
-  
+
 showAnswer.addEventListener('click', () => {
-  cardBack.classList.toggle('card__item__back');
+    cardBack.style.display = '';
 });
 
 nextCard.addEventListener('click', () => {
-  cardBack.classList.toggle('card__item__back');  
+  cardBack.style.display = 'none';
   cardIteration++;
   getData();
 });
