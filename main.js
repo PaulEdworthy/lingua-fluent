64e13be7ccf899dbe09f1d8b818706b90f0c0900
PaulEdworthy <pauledworthy@gmail.com>
2021-07-18 20:23:22 -0400

    build: saved state to local storage

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let number, french, english;
 let cardIteration = 0;
+let getState = localStorage.getItem('state');
+
 const jsonUrl = ('./1000_words.json');
 const cardBack = document.getElementById('card__item__back');
 const sourceLang = document.getElementById('source-language');
@@ -9,36 +11,48 @@ const nextCard = document.getElementById('btn--next');
 const hardChoice = document.getElementById('btn--hard');
 const goodChoice = document.getElementById('btn--good');
 const easyChoice = document.getElementById('btn--easy');
+// const saveState = document.getElementById('state__wrapper');
 
 cardBack.style.display = 'none';
 
-function getData () {
+onload = function () {
+  if (getState > 0) {
+    cardIteration = parseInt(getState);
+  } 
+}
+
+function getData() {
   fetch(jsonUrl)
-  .then(response => {
-    return response.json();
-  }).then(data => {
+    .then(response => {
+      return response.json();
+    }).then(data => {
 
-    // remove cardIteration and use random card
-    // let randomWord = Math.floor(Math.random() * 1000);
-    // console.log(randomWord);
+      // remove cardIteration and use random card
+      // let randomWord = Math.floor(Math.random() * 1000);
+      // console.log(randomWord);
 
-    number = data[cardIteration].Number;
-    french = data[cardIteration].French;
-    english = data[cardIteration].English;
+      number = data[cardIteration].Number;
+      french = data[cardIteration].French;
+      english = data[cardIteration].English;
 
-    // console.log(data[0].English);
-    sourceLang.textContent = french;
-    targetLang.textContent = english;
-  });
+      // console.log(data[0].English);
+      sourceLang.textContent = french;
+      targetLang.textContent = english;
+    });
 }
 getData();
 
 showAnswer.addEventListener('click', () => {
-    cardBack.style.display = '';
+  cardBack.style.display = '';
 });
 
 nextCard.addEventListener('click', () => {
   cardBack.style.display = 'none';
   cardIteration++;
+  saveCurrentState();
   getData();
 });
+
+function saveCurrentState() {
+  localStorage.setItem('state', cardIteration);
+}
